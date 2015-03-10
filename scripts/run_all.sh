a6dcdbdd29d3ca4f296a34d1f64aeb2255706928
Andrej Satara <andrej.satara@gmail.com>
2015-03-09 19:19:51 -0700

    Updating runall

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <script> <nodes file>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <nodes file>"
     exit
 fi
 
-if [ ! -f $2 ]; then
+if [ ! -f $1 ]; then
     echo "Must enter a valid file"
     exit
 fi
 while read p; do
-    ssh -o ConnectTimeout=10 -o StrictHostKeyChecking='no' ubc_eece411_2@$p $1 &
-done <nodes.txt
+    ssh -o ConnectTimeout=10 -o StrictHostKeyChecking='no' ubc_eece411_2@$p "nohup killall kvserver; ./kvserver > /dev/null 2>&1 &" &
+done <$1
